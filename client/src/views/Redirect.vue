5030ea60d4cd7d9e6b4e541e483a82d1f08ed3a3
Kozioleczek <wojciech.koziol@outlook.com>
2020-09-08 19:20:22 +0200

    Tweaks

--- client/src/views/Redirect.vue
+++ client/src/views/Redirect.vue
@@ -60,7 +60,7 @@ export default {
     this.$store.commit("setErrors", {});
     console.log(this.$router.currentRoute.params.redirect);
     // this.$store.dispatch('getRedirectUrl', { redirect: this.redirect });
-    // setTimeout(()=> window.location = this.$store.state.redirectUrl.long, 10000);
+    setTimeout(()=> window.location = this.$store.state.redirectUrl.long, 7000);
   },
   // beforeCreate() {
 
